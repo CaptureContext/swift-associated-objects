908caa26841cedcb7ed02ff4784d15896c05c97e
maximkrouk <id.maximkrouk@gmail.com>
2026-02-04 09:10:53 +0100

    feat: Update swift version

--- Package@swift-5.9.swift
+++ Package@swift-5.9.swift
@@ -1,4 +1,4 @@
-// swift-tools-version: 6.0
+// swift-tools-version: 5.9
 
 import PackageDescription
 import CompilerPluginSupport
@@ -70,5 +70,4 @@ let package = Package(
 			]
 		),
 	],
-	swiftLanguageModes: [.v6]
 )
